ed4c5673598e0832fcfb7f86a28b7ca4c82b40ad
Farid <mohammadizadeh79@gmail.com>
2022-08-24 07:10:53 +0430

    starting to video version

--- main.js
+++ main.js
@@ -1,17 +1,23 @@
 console.log("0w0");
-const biteCharecter = "@#W$975310!abc;:+=-,._ ";
+const biteCharecter = "@#W$975310!abc;:+=-,._  ";
 const biteLen = biteCharecter.length;
 
-let mainImg;
-function preload() {
-  mainImg = loadImage("/src/target.jpg");
-}
+// for images:
+  // let mainImg;
+  // function preload() {
+  //   mainImg = loadImage("/src/target.jpg");
+  // }
+// End:for images
+
+// for video:
+let videoSource ;
+
 
 function setup() {
-  createCanvas(400, 400);
-}
+  noCanvas();
+  videoSource = createCapture(VIDEO);
+  videoSource.size(48, 48);
 
-function draw() {
   background(10);
   // image(mainImg, 0, 0, width, height);
 
@@ -20,30 +26,39 @@ function draw() {
   
   mainImg.loadPixels();
 
-  for (let i = 0; i < mainImg.width; i++) {
-    for (let j = 0; j < mainImg.height; j++) {
+  for (let j = 0; j < mainImg.height; j++) {
+    let eachRow;
+    for (let i = 0; i < mainImg.width; i++) {
       const pixelIndex = (i + j * mainImg.width) * 4;
       const r = mainImg.pixels[pixelIndex + 0];
       const g = mainImg.pixels[pixelIndex + 1];
       const b = mainImg.pixels[pixelIndex + 2];
       // const a = mainImg.pixels[pixelIndex + 3];
+
       const average = (r + g + b) / 3;
 
-      noStroke();
-      let c = color(r, g, b);
-      fill(average);
-      // square(i * w, j * h, w * 0.4);
+    // before dom in p5:
+      // noStroke();
+      // let c = color(r, g, b);
+      // fill(average);
+    // End:before dom in p5
+
+
       const charindex = floor(
-        map(average+30, 0, 255, biteLen, 0)
+        map(average, 0, 255, biteLen, 0)
         );
-      
-      textSize(w);
-      textAlign(CENTER, CENTER);
+
+    // before dom in p5:
+      // textSize(w);
+      // textAlign(CENTER, CENTER);
       // text("Retr0", i * w + w * 0.5, j * h + h * 0.5);
-      text(biteCharecter.charAt(charindex), i * w + w * 0.5, j * h + h * 0.5);
+      // text(biteCharecter.charAt(charindex), i * w + w * 0.5, j * h + h * 0.5);
+    // End:before dom in p5
 
-
-
-    }
+    const eachCharecter = biteCharecter.charAt(charindex);
+    if (eachCharecter == " ") {eachRow += "&nbsp"};
+    eachRow += eachCharecter;
+  }
+  createDiv(eachRow.slice(9));
   }
 }
